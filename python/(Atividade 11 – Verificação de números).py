54dd0f4d11e13674d6f7be059afcd52a39cbd333
Vinicius oliveira <159170723+vini1912@users.noreply.github.com>
2024-05-14 15:35:32 -0300

    Delete python directory

--- python/(Atividade 11 – Verificação de números).py
+++
@@ -1,7 +0,0 @@
-n1 = int(input('digite um numero:'))
-resultado = n1 % 2
-
-if resultado == 1:
-    print ('esse numero é impar ')
-else:
-    print ('esse numero é par')
--- python/(Atividade 20 – Fatorial).py
+++
@@ -1,9 +0,0 @@
-numero = int(input("Fatorial de: ") )
-
-resultado=1
-f=1
-
-while f <= numero:
-    resultado *= f
-    f += 1
-    print(resultado)
